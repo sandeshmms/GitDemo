220c8cb3f18fbf2daf311abb72f62ef079f4929f
Sandesh <sms1982@gmail.com>
2020-12-11 13:52:19 +0530

    Modified BaseClass After method

--- src/test/java/testing/BaseClass.java
+++ src/test/java/testing/BaseClass.java
@@ -66,6 +66,7 @@ public class BaseClass
 		System.out.println("After Method called-4");
 		System.out.println("After Method called-5");
 		System.out.println("After Method called-6");
+		System.out.println("After Method called-7");
 	}
 
 }
