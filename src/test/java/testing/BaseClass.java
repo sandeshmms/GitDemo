0d392bc788bce5be41b5ae0f674d90cdf5cca3b1
Sandesh <sms1982@gmail.com>
2020-12-18 21:22:04 +0530

    Modified BaseClass AfterGroups method

--- src/test/java/testing/BaseClass.java
+++ src/test/java/testing/BaseClass.java
@@ -63,6 +63,7 @@ public class BaseClass
 	{
 		System.out.println("After Group called");
 		System.out.println("After Group called-1");
+		System.out.println("After Group called-2");
 	}
 
 	@BeforeMethod(onlyForGroups={"P1"}, alwaysRun=true)
