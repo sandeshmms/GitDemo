b4297182660a39b8e484e7c7e5adb97d56a13408
Sandesh <sms1982@gmail.com>
2020-12-19 20:57:39 +0530

    Modified BaseClass BeforeMethod method

--- src/test/java/testing/BaseClass.java
+++ src/test/java/testing/BaseClass.java
@@ -65,6 +65,7 @@ public class BaseClass
 		System.out.println("Before Method called-9");
 		System.out.println("Before Method called-10");
 		System.out.println("Before Method called-11");
+		System.out.println("Before Method called-12");
 	}
 
 	@AfterMethod(onlyForGroups={"P1"}, alwaysRun=true)
